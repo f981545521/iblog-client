fa3e49bdd1363303ca57d547fee6d1b3f1fe008c
youfang <981545521@qq.com>
2018-06-22 14:29:07 +0800

    add: spring cloud client

---
+++ src/main/java/cn/acyou/iblogclient/controller/HelloController.java
@@ -0,0 +1,28 @@
+package cn.acyou.iblogclient.controller;
+
+import cn.acyou.iblogclient.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-06-22 下午 01:37]
+ **/
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    @ResponseBody
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
+    }
+
+}
---
+++ src/main/java/cn/acyou/iblogclient/remote/HelloRemote.java
@@ -0,0 +1,16 @@
+package cn.acyou.iblogclient.remote;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-06-22 下午 01:41]
+ **/
+@FeignClient(name= "spring-cloud-producer")
+public interface HelloRemote {
+    @RequestMapping(value = "/hello/hello")
+    String hello(@RequestParam(value = "name") String name);
+}
