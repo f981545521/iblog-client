79d404a183801579c180d0086570eed162bf3eaa
youfang <981545521@qq.com>
2018-06-22 12:43:22 +0800

    init import
    init import

---
+++ src/main/java/cn/acyou/iblogclient/IblogClientApplication.java
@@ -0,0 +1,12 @@
+package cn.acyou.iblogclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IblogClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IblogClientApplication.class, args);
+	}
+}
